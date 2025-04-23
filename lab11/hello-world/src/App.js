f22d58c85cf4ea6666e30219d7a28306257c8e36
GitHuberea <annielin930930@gmail.com>
2025-04-23 18:55:01 +0800

    fix: bug

--- lab11/hello-world/src/App.js
+++ lab11/hello-world/src/App.js
@@ -1,29 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; 
 
-const styleArgument = { fontSize: '100px', color: 'Red' };
+const styleArgument = { fontSize: '100px', color: 'red' };
+
 const changeText=(event)=>{
-console.log(event.target)
-event.target.innerText = event.target.innerText + "被點了"
+  console.log(event.target)
+  event.target.innerText = event.target.innerText + "被點了"
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <h1 style = { styleArgument } onClick= {changeText}> hello CGU!!
       </h1>
     </div>
